23dd8851d0555d2ef648358489634a03d18a474d
jogeshgupta963 <jogeshgupta963@gmail.com>
2022-03-23 16:32:50 +0530

    chat app complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, { Fragment, useEffect, useState } from 'react';
+import Chat from './components/Chat'
 import './App.css';
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:80/')
+
 
 function App() {
+
+  const [name, setName] = useState("")
+  const [room, setRoom] = useState("")
+  const [join, setJoin] = useState(0)
+
+
+  const nameChangeHandle = (e) => {
+    try {
+      setName(e.target.value)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  const roomChangeHandle = (e) => {
+    try {
+      setRoom(e.target.value)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  const joinClickHandle = (e) => {
+    e.preventDefault();
+    setJoin(1);
+    if (room !== "" && name !== "") {
+      socket.emit('join_room', { name, room })
+    }
+    console.log('clicked button')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        {join === 0 && <div className="joinChatContainer">
+          <h1>Join A Room</h1>
+          <input type="text" placeholder='name...' onChange={nameChangeHandle} />
+          <input type="text" placeholder='Room Id...' onChange={roomChangeHandle} />
+          <button className="enterRoom" onClick={joinClickHandle}>Join</button>
+        </div>}
+        {join === 1 && <Chat socket={socket} name={name} room={room} />}
+        {/* <Chat socket={socket} name={name} room={room} /> */}
+      </div>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/Chat.js
@@ -0,0 +1,73 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import ScrollToBottom from 'react-scroll-to-bottom'
+function Chat({ socket, name, room }) {
+
+    const [input, setInput] = useState("")
+    const [messageList, setMessageList] = useState([])
+
+    const inputMsgChangeHandle = e => {
+        setInput(e.target.value)
+    }
+    const clickHandle = async (e) => {
+        e.preventDefault();
+
+        if (input !== "") {
+            const msgData = {
+                room: room,
+                author: name,
+                message: input,
+                time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+            }
+            await socket.emit('send_message', msgData)
+            setMessageList(list => [...list, msgData])
+            setInput("")
+        }
+
+    }
+    useEffect(() => {
+        socket.on('recieve_message', data => {
+            // let list = [...messageList, data];
+            setMessageList(list => [...list, data])
+            console.log(messageList)
+        })
+    }, [socket])
+    return (
+        <Fragment>
+            <div className='chat-window'>
+                <div className="chat-header">Live Chat</div>
+
+
+                <div className="chat-body">
+                    <ScrollToBottom className="message-container">
+                        {messageList.map(msg => {
+                            return <div className='message' id={name !== msg.author ? 'you' : 'other'}>
+                                <div>
+                                    <div className="message-content">
+                                        <p>{msg.message}</p>
+                                    </div>
+                                    {/* <div className="message-content">
+                                        <p >{msg.time}</p>
+                                        <p >{msg.author}</p>
+                                    </div> */}
+                                    <div >
+                                        <p id="time">{msg.time}</p>
+                                        <p id="author">{msg.author}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        })}
+                    </ScrollToBottom>
+
+                </div>
+
+
+                <div className="chat-footer">
+                    <input type="text" value={input} placeholder='heyy...' onChange={inputMsgChangeHandle} />
+                </div>
+                <button className="btn-send" onKeyPress={e => { if (e.key === 'Enter') clickHandle() }} onClick={clickHandle}>▶️</button>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Chat
